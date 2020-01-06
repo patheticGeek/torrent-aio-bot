7302549e6612dfd8780544e6402139f41b08ab06
patheticGeek <shivamnagi14@gmail.com>
2020-01-06 16:17:34 +0530

    bot update

--- bot/index.js
+++ bot/index.js
@@ -1,12 +1,36 @@
 const Telegram = require("node-telegram-bot-api");
+const fetch = require("isomorphic-unfetch");
 const dev = process.env.NODE_ENV !== "production";
-const token = require("./token");
+const token = dev ? require("./token") : process.env.TELEGRAM_TOKEN;
+const api = "https://torrent-aio-bot.herokuapp.com/api/v1";
 
 const bot = new Telegram(token, { polling: true });
 
-bot.on("message", function(msg) {
-  var chatId = msg.chat.id;
-  bot.sendMessage(chatId, "Recived.");
+bot.onText(/\/echo (.+)/, function(msg, match) {
+  var fromId = msg.from.id;
+  var resp = match[1];
+
+  bot.sendMessage(fromId, `you wrote: ${resp}`);
+});
+
+bot.onText(/\/search piratebay (.+)/, async (msg, match) => {
+  var from = msg.from.id;
+  var query = match[1];
+
+  bot.sendMessage(from, "Searching...");
+
+  const data = await fetch(api + "/search/piratebay?query=" + query);
+  if (data.error) {
+    bot.sendMessage(from, "An error occured on server");
+  } else if (data.totalResults === 0) {
+    bot.sendMessage(from, "No results found.");
+  } else if (data.results.lenght > 0) {
+    let results = "";
+    data.results.forEach(result => {
+      results += `Name: ${result.name} \nSeeds: ${result.seeds} \nDetails: ${result.details} \nLink: ${result.link} \n\n`;
+    });
+    bot.sendMessage(from, results);
+  }
 });
 
 module.exports = bot;
