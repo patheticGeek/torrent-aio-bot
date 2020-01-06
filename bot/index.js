5f2451efcce23f64fce2fddc08472f23f7fde691
patheticGeek <shivamnagi14@gmail.com>
2020-01-06 15:11:21 +0530

    telegram-bot-api test 2

--- bot/index.js
+++ bot/index.js
@@ -4,11 +4,6 @@ const token = require("./token");
 
 const bot = new Telegram(token, { polling: true });
 
-if (!dev) {
-  console.log("using webhook");
-  bot.setWebHook("https://torrent-aio-bot.herokuapp.com/torrent-bot");
-}
-
 bot.on("message", function(msg) {
   var chatId = msg.chat.id;
   bot.sendMessage(chatId, "Recived.");
