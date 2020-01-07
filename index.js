df84a0391569cb60beefc49c9e42f61d8e0237a4
patheticGeek <shivamnagi14@gmail.com>
2020-01-07 16:25:17 +0530

    refactoring complete

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const next = require("next");
 const compression = require("compression");
-const WebTorrent = require("webtorrent");
 require("./lib/bot");
 
-const keepalive = require("./utils/keepalive");
-const prettyBytes = require("./utils/prettyBytes");
 const humanTime = require("./utils/humanTime");
+const keepalive = require("./utils/keepalive");
 const diskinfo = require("./utils/diskinfo");
 const status = require("./utils/status");
 
 const search = require("./routes/search");
 const details = require("./routes/details");
+const torrent = require("./routes/torrent");
 
 const dev = process.env.NODE_ENV !== "production";
 const PORT = parseInt(process.env.PORT, 10) || 3000;
@@ -20,31 +19,7 @@ const server = express();
 const app = next({ dev, dir: "web" });
 const handle = app.getRequestHandler();
 
-const torrentClient = new WebTorrent();
-
 keepalive();
-
-const statusLoader = torrent => {
-  return {
-    magnetURI: torrent.magnetURI,
-    name: torrent.name,
-    speed: `${prettyBytes(torrent.downloadSpeed)}/s`,
-    downloaded: prettyBytes(torrent.downloaded),
-    total: prettyBytes(torrent.length),
-    progress: parseInt(torrent.progress * 100),
-    timeRemaining: parseInt(torrent.timeRemaining),
-    redableTimeRemaining: humanTime(torrent.timeRemaining),
-    done: torrent.done,
-    files: torrent.files.map(file => ({
-      name: file.name,
-      downloaded: prettyBytes(file.downloaded),
-      total: prettyBytes(file.length),
-      progress: parseInt(file.progress * 100),
-      done: file.done,
-      path: file.path
-    }))
-  };
-};
 
 (async () => {
   await app.prepare();
@@ -55,8 +30,14 @@ const statusLoader = torrent => {
 
   server.use("/api/v1/details", details);
 
+  server.use("/api/v1/torrent", torrent);
+
   server.get("/ping", (req, res) => {
     res.send("pong");
+  });
+
+  server.get("/api/v1/uptime", async (req, res) => {
+    res.send({ uptime: humanTime(process.uptime() * 1000) });
   });
 
   server.get("/api/v1/diskinfo", async (req, res) => {
@@ -68,78 +49,6 @@ const statusLoader = torrent => {
   server.get("/api/v1/status", async (req, res) => {
     const currStatus = await status();
     res.send(currStatus);
-  });
-
-  server.get("/api/v1/torrent/download", async (req, res) => {
-    const link = req.query.link;
-    const index = req.query.index;
-    if (!link || !index) {
-      res.send({ error: true, errorMessage: "No link and/or index provided" });
-    } else {
-      const torrent = torrentClient.get(link);
-      if (torrent) {
-        if (!torrent.files[index].done) {
-          res.send({
-            error: true,
-            errorMessage: "It hasent finished downloading yet",
-            link
-          });
-        } else {
-        }
-      } else {
-        res.send({
-          error: true,
-          errorMessage: "No such torrent exists",
-          link
-        });
-      }
-    }
-  });
-
-  server.get("/api/v1/torrent/start", (req, res) => {
-    const link = req.query.link;
-    if (!link) {
-      res.send({ error: true, errorMessage: "No link provided" });
-    } else {
-      if (!torrentClient.get(link)) {
-        torrentClient.add(link);
-      }
-      res.send({ error: false, link });
-    }
-  });
-
-  server.get("/api/v1/torrent/remove", (req, res) => {
-    const link = req.query.link;
-    if (!link) {
-      res.send({ error: true, errorMessage: "No link provided" });
-    } else {
-      if (torrentClient.get(link)) {
-        torrentClient.remove(link);
-      }
-      res.send({ error: false });
-    }
-  });
-
-  server.get("/api/v1/torrent/status", (req, res) => {
-    const link = req.query.link;
-    let torrent = null;
-    if (torrentClient.get(link)) {
-      torrent = torrentClient.get(link);
-      res.send({ error: false, link, status: statusLoader(torrent) });
-    } else {
-      res.send({ error: true, errorMessage: "No such torrent exists", link });
-    }
-  });
-
-  server.get("/api/v1/torrent/list", (req, res) => {
-    const torrents = torrentClient.torrents.map(torrent =>
-      statusLoader(torrent)
-    );
-    res.send({
-      error: false,
-      totalDownloadSpeed: torrentClient.downloadSpeed,
-      torrents
-    });
   });
 
   server.all("*", (req, res) => {
---
+++ routes/torrent.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const Torrent = require("../lib/torrent");
+const torrent = new Torrent();
+
+const router = express.Router();
+
+router.get("/start", (req, res) => {
+  const link = req.query.link;
+  if (!link) {
+    res.send({ error: true, errorMessage: "No link provided" });
+  } else {
+    torrent.addTorrent(link);
+    res.send({ error: false, link });
+  }
+});
+
+router.get("/remove", (req, res) => {
+  const link = req.query.link;
+  if (!link) {
+    res.send({ error: true, errorMessage: "No link provided" });
+  } else {
+    if (torrent.getTorrent(link)) {
+      torrent.removeTorrent(link);
+    }
+    res.send({ error: false, link });
+  }
+});
+
+router.get("/status", (req, res) => {
+  const link = req.query.link;
+  if (!link) {
+    res.send({ error: true, errorMessage: "No link provided" });
+  } else {
+    res.send({ error: false, status: torrent.getTorrent(link) });
+  }
+});
+
+router.get("/list", (req, res) => {
+  try {
+    res.send({ error: false, torrents: torrent.listTorrents() });
+  } catch (err) {
+    console.log(err);
+    res.send({ error: true, errorMessage: err.message });
+  }
+});
+
+module.exports = router;
