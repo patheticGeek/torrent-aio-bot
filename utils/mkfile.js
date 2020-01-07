75453a3b3937deb21405da72c712b878ad0fc7e3
patheticGeek <shivamnagi14@gmail.com>
2020-01-07 21:02:01 +0530

    save file to disk done

---
+++ utils/mkfile.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+function mkfile(file) {
+  file = file.split("/");
+  const fileName = file.pop();
+  file.forEach((val, i) => {
+    if (val !== ".") {
+      const currPath = file.slice(0, i + 1).join("/");
+      if (!fs.existsSync(currPath)) {
+        fs.mkdirSync(currPath);
+      }
+    }
+  });
+  file = file.join("/") + "/" + fileName;
+  fs.writeFileSync(file);
+}
+
+module.exports = mkfile;
