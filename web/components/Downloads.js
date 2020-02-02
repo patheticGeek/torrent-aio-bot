46dbc59696a4ca4ebf3fd2a6abdead30aefe13f6
patheticGeek <shivamnagi14@gmail.com>
2020-02-02 17:40:43 +0530

    bot and api finalized

---
+++ web/components/Downloads.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Downloads extends Component {
+  render() {
+    return (
+      <main>
+        <h1>Downloads</h1>
+      </main>
+    );
+  }
+}
+
+export default Downloads;
---
+++ web/components/Search.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Search extends Component {
+  render() {
+    return (
+      <main>
+        <h1>Search</h1>
+      </main>
+    );
+  }
+}
+
+export default Search;
