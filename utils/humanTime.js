7ac86eb9ff16bd5b4f935dd6631d76ed36c065ca
patheticGeek <shivamnagi14@gmail.com>
2020-02-18 21:18:27 +0530

    minor update

--- utils/humanTime.js
+++ utils/humanTime.js
@@ -2,14 +2,14 @@ function humanTime(ms) {
   let seconds = ms / 1000;
   let result = "";
   const days = Math.floor((seconds % 31536000) / 86400);
-  if (days > 0) result += `${days} days `;
+  if (days > 0) result += `${days} d `;
   const hours = Math.floor(((seconds % 31536000) % 86400) / 3600);
-  if (hours > 0) result += `${hours} hours `;
+  if (hours > 0) result += `${hours} h `;
   const minutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60);
-  if (minutes > 0) result += `${minutes} minutes `;
+  if (minutes > 0) result += `${minutes} m `;
   seconds = ((((seconds % 31536000) % 86400) % 3600) % 60).toFixed(0);
-  if (seconds > 0) result += `${seconds} seconds`;
-  if (result === "") result += "finished";
+  if (seconds > 0) result += `${seconds} s`;
+  if (result === "") result += "0s";
   return result;
 }
 
--- web/pages/index.js
+++ web/pages/index.js
@@ -4,7 +4,7 @@ import Search from "../components/Search";
 import Downloads from "../components/Downloads";
 
 class Index extends Component {
-  state = { nav: "downloads", searchProps: null };
+  state = { nav: "search", searchProps: null };
 
   static getInitialProps = async ({ query }) => {
     if (!query.term || !query.site || process.browser) return {};
