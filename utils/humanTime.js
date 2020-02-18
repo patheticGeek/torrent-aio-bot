f83b357d51087f73722e01aca2cede76a607fc1f
patheticGeek <shivamnagi14@gmail.com>
2020-02-18 22:11:38 +0530

    time notation update

--- utils/humanTime.js
+++ utils/humanTime.js
@@ -2,13 +2,13 @@ function humanTime(ms) {
   let seconds = ms / 1000;
   let result = "";
   const days = Math.floor((seconds % 31536000) / 86400);
-  if (days > 0) result += `${days} d `;
+  if (days > 0) result += `${days}d `;
   const hours = Math.floor(((seconds % 31536000) % 86400) / 3600);
-  if (hours > 0) result += `${hours} h `;
+  if (hours > 0) result += `${hours}h `;
   const minutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60);
-  if (minutes > 0) result += `${minutes} m `;
+  if (minutes > 0) result += `${minutes}m `;
   seconds = ((((seconds % 31536000) % 86400) % 3600) % 60).toFixed(0);
-  if (seconds > 0) result += `${seconds} s`;
+  if (seconds > 0) result += `${seconds}s`;
   if (result === "") result += "0s";
   return result;
 }
