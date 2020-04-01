d6bf45dcb786ac46c73695e97cddf2f61251d576
Pathetic Geek <geekpathetic@gmail.com>
2020-04-01 21:20:51 +0530

    test route added

--- routes/search.js
+++ routes/search.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const puppeteer = require("puppeteer");
 
 const piratebaySearch = require("../crawllers/piratebay/search");
 const o337xSearch = require("../crawllers/1337x/search");
 const limetorrentSearch = require("../crawllers/limetorrent/search");
 
 const router = express.Router();
+
+router.get("/test", async (req, res) => {
+  try {
+    const browser = await puppeteer.launch();
+
+    await browser.close();
+    res.send({ error: false });
+  } catch (e) {
+    console.log("Puppeteer error");
+    res.json({ error: true, errorMessage: e.message });
+  }
+});
 
 router.get("/piratebay", async (req, res) => {
   let query = req.query.query;
