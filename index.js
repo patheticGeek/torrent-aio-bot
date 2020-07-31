c1e7172cf77ff29ade2679c87cb2208eb77059d9
Wrench <65120517+JyothisJayanth@users.noreply.github.com>
2020-07-31 21:00:11 +0530

    Fix Typo

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ server.use((req, res, next) => {
   next();
 });
 
-server.get("/ping", (req, res) => res.send("pong"));
+server.get("/ping", (req, res) => res.send("Pong!"));
 
 server.get("/logs", (req, res) => res.sendFile("logs.txt", { root: __dirname }));
 
