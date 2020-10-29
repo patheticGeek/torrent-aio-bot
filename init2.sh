af1f01ad6fc815612fe49c084a72b286443fda35
dag <daggergad7@gmail.com>
2020-10-29 21:41:39 +0530

    fix heroku crash

--- init2.sh
+++ init2.sh
@@ -5,8 +5,10 @@ wget -q https://downloads.rclone.org/v1.51.0/rclone-v1.51.0-linux-amd64.zip
 unzip -q rclone-v1.51.0-linux-amd64.zip
 export PATH=$PWD/rclone-v1.51.0-linux-amd64:$PATH
 
-if [[ -n $RCLONE_CONFIG ]]; then
+if [[ -n $RCLONE_CONFIG && -n $RCLONE_DESTINATION ]]; then
 	echo "Rclone config detected"
 	echo -e "$RCLONE_CONFIG" > rclone.conf
 	chmod +x rclone_up.sh
 fi
+
+node index.js
